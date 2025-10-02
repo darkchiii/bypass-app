6575e19db13e688ec2364c292436afe1649fa947
Darusia <61788990+darkchiii@users.noreply.github.com>
2025-10-02 10:54:12 +0200

    Fix converting cv data to bytes-like object

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
 from . import cv_parser
+import io
 # import cv_parser
 
 app = FastAPI()
@@ -14,30 +15,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.post("/upload_cv")
-# async def upload_cv(file: UploadFile = File(...)):
+@app.post("/api/upload-cv")
 async def upload_cv(file: Annotated[UploadFile, File()]):
-    # Validate file type
     if not file.content_type == "application/pdf":
         raise HTTPException(status_code=400, detail="Only PDF files are allowed")
 
     try:
         content = await file.read()
 
-        # Convert bytes to file-like object for pdfplumber
-        # pdf_file = io.BytesIO(content)
-        text = cv_parser.extract_text_from_pdf(content)
-        sections = cv_parser.parse_cv_sections(text)
+        pdf_file = io.BytesIO(content)
+        pdf_file.read()
+        text = cv_parser.extract_text_from_pdf(pdf_file)
 
-        return {"status": "success", "cv_data": sections}
+        return {"status": "success", "cv_data": text}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing CV: {str(e)}")
 
     finally:
-        # Reset file pointer for potential reuse
         await file.seek(0)
+
+# @app.post("/api/save-base-cv/")
+# async def save_base_cv(cv_data: ParsedCV):
+
+#     return {"status": "saved"}
+
+# @app.get("/api/get-base-cv")
+# async def get_base_cv():
 
 
 @app.get("/")
